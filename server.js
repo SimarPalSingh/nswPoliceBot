ca63115b55f3d7f132c7aa48f9b3d9c9f2e567da
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-26 16:45:47 +1100

    w

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000 );//required
 
-app.get('*', function(res){
+app.get('/', function(res){
     res.write(JSON.stringify({"random": Math.random()*9999}));
 });
