9e1c5e6a1221eb787aab39e6215dab5d30a506e8
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-26 16:51:35 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 // app.set('port', process.env.PORT || 3000 );//required
 app.listen(process.env.PORT || 3000);
-app.get('/', function(res){
+app.get('/getRandom', function(res){
     res.send(JSON.stringify({"random": Math.random()*9999}));
 });
