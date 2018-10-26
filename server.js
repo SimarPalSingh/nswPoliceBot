afcac9171bb0d6ae96e1ce19c376f4446104f201
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-26 16:32:09 +1100

    q

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+app.set('port', process.env.PORT || 3000 );//required
 
 app.get('*', function(res){
     res.write(JSON.stringify({"random": Math.random()*9999}));
-})
+});
