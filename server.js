40251c16c00e64ac962eec7226c2b5ccaaeec73d
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-26 17:01:49 +1100

    e

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = express();
 // app.set('port', process.env.PORT || 3000 );//required
 app.listen(process.env.PORT || 3000);
 app.get('/getRandom', function(res){
-    res.send(JSON.stringify({"random": Math.random()*9999}));
+    res.send(JSON.stringify({random: 'Math.random()*9999'}));
 });
