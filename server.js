9f544e59712811f14257f27a3b68237851657d7d
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-29 13:54:34 +1100

    sd

--- server.js
+++ server.js
@@ -2,14 +2,30 @@
 var rp = require('request-promise');
 var express = require('express');
 var app = express();
+var body_parser = require('body-parser');
+
+app.use(body_parser.urlencoded({ extended: false }));    // for GET requeests
+app.use(body_parser.json()); 
 // app.set('port', process.env.PORT || 3000 );//required
+function rp_async(url,options) {
+    return new Promise((resolve,reject) => {
+        rp(url,options).then(function (json_string) {
+            resolve(json_string);
+        }).catch(function (err){
+            console.log(err);
+            reject("ERROR");
+        });
+    });
+}
+
 app.listen(process.env.PORT || 3000);
 app.get('/getRandom', function(req,res){
     res.send(JSON.stringify({"random": Math.round(Math.random()*10000)}));
 });
 
 app.post('/getNearestLocations', async function (req, res) {
-    var stringToSearch = JSON.parse(req.body).data;
+    console.log(req.body);
+    var stringToSearch = req.body.data;
  
     var data = await rp_async(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=~${stringToSearch}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBoJFhnn_8nwQltsj496SeNCN4lk03qf9w`,{method:'get'});
     data = JSON.parse(data);
