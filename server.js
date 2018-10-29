1feb1efcc01ed637627e7ff3d7ee2f6005baaf10
SimarPalSingh <43463524+SimarPalSingh@users.noreply.github.com>
2018-10-29 13:28:52 +1100

    h

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ var app = express();
 // app.set('port', process.env.PORT || 3000 );//required
 app.listen(process.env.PORT || 3000);
 app.get('/getRandom', function(req,res){
-    res.send(JSON.stringify({"random": Math.round(Math.random()*1000000)}));
+    res.send(JSON.stringify({"random": Math.round(Math.random()*10000)}));
+});
+
+app.post('/getNearestLocations', async function (req, res) {
+    var stringToSearch = req.body.data;
+ 
+    var data = await rp_async(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=~${stringToSearch}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBoJFhnn_8nwQltsj496SeNCN4lk03qf9w`,{method:'get'});
+    data = JSON.parse(data);
+    
+    res.send(`Location obtained. \n${JSON.stringify(data)}`);
 });
